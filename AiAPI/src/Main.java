1c8ac69cd68b116e5010035a0eef29ce40737a05
jacksonfava <jackson_fava28@milton.edu>
2026-01-09 12:06:21 -0500

    found a good api for my idea

---
+++ AiAPI/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//get dc and marvel apis plus power scaling for fighting e bit game thingy
+
+// found some good api's
+
+//https://akabab.github.io/superhero-api/api/all.json
